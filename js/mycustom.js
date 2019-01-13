06cf726ac60e73a0efce6a13f813e7528b130a16
ryeol2 <ryeol22e@gmail.com>
2019-01-13 20:08:12 +0900

    Create mycustom.js

---
+++ js/mycustom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('#logojava').mouseover(function(){
+    $(this).css({"filter": "brightness(50%)"});
+  });
+  $('#logojava').mouseout(function(){
+    $(this).css({"filter": "brigthness(100%)"});
+  });
+
+  $('#logospring').mouseover(function(){
+    $(this).css({"filter": "brightness(50%)"});
+  });
+  $('#logospring').mouseout(function(){
+    $(this).css({"filter": "brigthness(100%)"});
+  });
+
+  $('#logojs').mouseover(function(){
+    $(this).css({"filter": "brightness(50%)"});
+  });
+  $('#logojs').mouseout(function(){
+    $(this).css({"filter": "brigthness(100%)"});
+  });
+
+  $('#logosql').mouseover(function(){
+    $(this).css({"filter": "brightness(50%)"});
+  });
+  $('#logosql').mouseout(function(){
+    $(this).css({"filter": "brigthness(100%)"});
+  });
+});

c87ff78f510bd4db420cbcafc00060ab5184f6cc
ryeol2 <ryeol22e@gmail.com>
2019-01-13 20:11:35 +0900

    Update mycustom.js

--- js/mycustom.js
+++ js/mycustom.js
@@ -3,27 +3,27 @@ $(document).ready(function(){
     $(this).css({"filter": "brightness(50%)"});
   });
   $('#logojava').mouseout(function(){
-    $(this).css({"filter": "brigthness(100%)"});
+    $(this).css({"filter": "brightness(100%)"});
   });
 
   $('#logospring').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
   });
   $('#logospring').mouseout(function(){
-    $(this).css({"filter": "brigthness(100%)"});
+    $(this).css({"filter": "brightness(100%)"});
   });
 
   $('#logojs').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
   });
   $('#logojs').mouseout(function(){
-    $(this).css({"filter": "brigthness(100%)"});
+    $(this).css({"filter": "brightness(100%)"});
   });
 
   $('#logosql').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
   });
   $('#logosql').mouseout(function(){
-    $(this).css({"filter": "brigthness(100%)"});
+    $(this).css({"filter": "brightness(100%)"});
   });
 });

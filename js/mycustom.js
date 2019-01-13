aea38fab5a84ae0bb72d9651424d61f1a0a04efe
ryeol2 <ryeol22e@gmail.com>
2019-01-13 20:43:47 +0900

    Update mycustom.js

--- js/mycustom.js
+++ js/mycustom.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
   $('#logojava').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
-    $('.text-muted mb-0').fadeIn(1000);
+    $('#java').fadeIn(1000);
   });
   $('#logojava').mouseout(function(){
     $(this).css({"filter": "brightness(100%)"});
-      $('.text-muted mb-0').fadeOut(1000);
+      $('#java').fadeOut(1000);
   });
 
   $('#logospring').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
-      $('.text-muted mb-0').fadeIn(1000);
+      $('#spring').fadeIn(1000);
   });
   $('#logospring').mouseout(function(){
     $(this).css({"filter": "brightness(100%)"});
-      $('.text-muted mb-0').fadeOut(1000);
+      $('#spring').fadeOut(1000);
   });
 
   $('#logojs').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
-      $('.text-muted mb-0').fadeIn(1000);
+      $('#js').fadeIn(1000);
   });
   $('#logojs').mouseout(function(){
     $(this).css({"filter": "brightness(100%)"});
-      $('.text-muted mb-0').fadeOut(1000);
+      $('#js').fadeOut(1000);
   });
 
   $('#logosql').mouseover(function(){
     $(this).css({"filter": "brightness(50%)"});
-      $('.text-muted mb-0').fadeIn(1000);
+      $('#sql').fadeIn(1000);
   });
   $('#logosql').mouseout(function(){
     $(this).css({"filter": "brightness(100%)"});
-      $('.text-muted mb-0').fadeOut(1000);
+      $('#sql').fadeOut(1000);
   });
 
 
